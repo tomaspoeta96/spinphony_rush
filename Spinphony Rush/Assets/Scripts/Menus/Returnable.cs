398ca34bfbb68e109e224e5645f82d6aa25b5391
a13monly <a13monly@gmail.com>
2019-11-14 16:00:18 +0100

    Avance Menus
    Main Menu hecho, desde el cual se puede accceder a Options, Phony selection, Phonies o salir del juego

---
+++ Spinphony Rush/Assets/Scripts/Menus/Returnable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Returnable : MonoBehaviour
+{
+    public KeysTable KeysTable;
+    public Button BackButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackButton.onClick.AddListener(Return);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape)){
+            Return();
+        }
+    }
+
+    // Update is called once per frame
+    void Return()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
