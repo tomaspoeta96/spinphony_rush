d7b6b99f83ed27ce1dfc030284c3a0851ea8a68b
a13monly <a13monly@gmail.com>
2019-11-21 16:26:48 +0100

    Menu Selection y Pausa
    Avances del menu Phony Selection.
    Creación del Menu Pausa.

---
+++ Spinphony Rush/Assets/Scripts/Menus/PhonySelectioner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhonySelectioner : MonoBehaviour
+{
+    public Sprite[] peonzas;
+    public GameObject peonza;
+    public Button rightButton, leftButton;
+
+    private int currentSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSprite = 0;
+        peonza.GetComponent<Image>().sprite = peonzas[currentSprite];
+        rightButton.onClick.AddListener(OnLeft);
+        leftButton.onClick.AddListener(OnRight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        peonza.GetComponent<Image>().sprite = peonzas[currentSprite];
+    }
+
+    private void OnLeft()
+    {
+        if (peonza.GetComponent<Image>().sprite == peonzas[0])
+            currentSprite = peonzas.Length - 1;
+        else currentSprite--;
+    }
+
+    private void OnRight()
+    {
+        if (peonza.GetComponent<Image>().sprite == peonzas[peonzas.Length - 1])
+            currentSprite = 0;
+        else currentSprite++;
+    }
+
+
+
+}
