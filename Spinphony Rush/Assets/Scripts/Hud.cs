5650732adae260473ec6888c48fcbd5dfdae8862
Aitana Villaplana <aitanagirlpotter@gmail.com>
2019-11-24 11:20:00 +0100

    HUD player1 y 2
    Hud basico jugadores 1 y 2, sistema de puntuación básico

---
+++ Spinphony Rush/Assets/Scripts/Hud.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    public Image boost;
+    public Text name;
+    public Text text_points;
+    public PhonyPlayerController phony;
+    public Material jump;
+    public Material shield;
+    public Material fuelle;
+    public Material move;
+
+    private float survive = 0f;
+    private float repeatTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boost.enabled = false;
+        text_points.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        survive += Time.deltaTime;
+        controllerBoost();
+        viewBoost();
+        incresePoints();
+    }
+
+    void controllerBoost() {
+        if(phony.haveJump || phony.haveShield || phony.haveFuelle || phony.haveMove)
+            boost.enabled = true;
+        if(!phony.haveJump && !phony.haveShield && !phony.haveFuelle && !phony.haveMove)
+            boost.enabled = false;
+    }
+
+    void incresePoints() {
+        if (survive >= repeatTime) {
+            survive = 0f;
+            if(!phony.muerto) phony.points += 50;
+        }
+        text_points.text = (phony.points).ToString();
+    }
+
+    void viewBoost() {
+        if(phony.haveJump)
+            boost.material = jump;
+        if(phony.haveShield)
+            boost.material = shield;
+        if(phony.haveFuelle)
+            boost.material = fuelle;
+        if(phony.haveMove)
+            boost.material = move;
+    }
+        
+}
