61b3637383d699c6f5550d5f7f75e310a5ca9993
a13monly <a13monly@gmail.com>
2019-11-26 03:34:01 +0100

    Map Selection
    Escena de selección de mapa

---
+++ Spinphony Rush/Assets/Scripts/Menus/MapSelection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MapSelection : MonoBehaviour
+{
+    public Button[] maps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            maps[0].onClick.AddListener(RandomMap);
+            maps[1].onClick.AddListener(ActionMap1);
+            maps[2].onClick.AddListener(ActionMap2);
+    }
+
+    private void RandomMap()
+    {
+        SceneManager.LoadScene("Map" + UnityEngine.Random.Range(1, maps.Length));
+    }
+
+    private void ActionMap1()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+
+    private void ActionMap2()
+    {
+        SceneManager.LoadScene("Map2");
+    }
+
+}
