93f33565e3e6391ec158f995dfeaea8a2dd01cce
Aitana Villaplana <aitanagirlpotter@gmail.com>
2020-01-13 23:35:11 +0100

    Mapa Espacio completo

--- Spinphony Rush/Assets/Scripts/Maps/MapSpace.cs
+++ Spinphony Rush/Assets/Scripts/Maps/MapSpace.cs
@@ -13,6 +13,6 @@ public class MapSpace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Rotate(0f, 0f, -0.3f, Space.Self);
     }
 }
